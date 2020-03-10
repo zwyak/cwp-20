a1e128450b9f1de70e92647bc30580346a361181
zwyak <vlad.senyuk97@gmail.com>
2020-03-10 16:02:29 +0300

    Added countries render

--- index.js
+++ index.js
@@ -12,8 +12,18 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/countries.html', async (req, res) => {
-  const countries = await db.country.findAll({raw: true, limit: 25});
-  await res.render('countries', { countries: countries});
+  let page = 0;
+  if (req.query.page) page = req.query.page;
+  const offset = page * 25;
+  const limit = 25;
+  const countriesCount = await db.country.count();
+  const totalPageCount = Math.floor((countriesCount + limit - 1) / limit);
+  let pages = [];
+  for (let i = 0; i < totalPageCount; i++) {
+    pages[i] = i;
+  }
+  const countries = await db.country.findAll({raw: true, limit: limit, offset: offset});
+  await res.render('countries', { countries: countries, pages: pages, currentPage: page, offset: offset, count: countries.length});
 });
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
