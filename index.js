81e590ab094d9dc332137ba0115bf6adba8f421c
zwyak <vlad.senyuk97@gmail.com>
2020-03-10 10:55:37 +0300

    Added countries pug

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.get('/', async (req, res) => {
-  let countriesCount = await db.country.count();
-  let citiesCount = await db.city.count();
-
+  const countriesCount = await db.country.count();
+  const citiesCount = await db.city.count();
   await res.render('index', { countriesCount: countriesCount, citiesCount: citiesCount });
+});
+
+app.get('/countries.html', async (req, res) => {
+  const countries = await db.country.findAll({raw: true, limit: 25});
+  await res.render('countries', { countries: countries});
 });
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
