8ba41fa18726aaee156a2532bf7af3b43e13f433
zwyak <vlad.senyuk97@gmail.com>
2020-03-10 10:10:26 +0300

    Rendered index.pug

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const Sequelize = require('sequelize');
 const db = require('./models')(Sequelize);
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', async (req, res) => {
+  let countriesCount = await db.country.count();
+  let citiesCount = await db.city.count();
+
+  await res.render('index', { countriesCount: countriesCount, citiesCount: citiesCount });
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000!'));
